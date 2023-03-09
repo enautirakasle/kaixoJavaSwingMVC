53525e898c5674171c9edb218c0db98b8af6caf6
Eñaut Agirre Arri <enaut.irakasle@gmail.com>
2023-03-09 08:17:50 +0100

    documentar y comentar este ejemplo

--- src/controlador/ControladorPrincipal.java
+++ src/controlador/ControladorPrincipal.java
@@ -6,23 +6,54 @@ import java.awt.event.ActionListener;
 import modelo.ProductoModelo;
 import vista.Principal;
 import vista.ProductoFormulario;
-
+/**
+ * Escuchara eventos de los componentes del jframe vista.Principal
+ * y ejecutará las acciones de los métodos.
+ * 
+ * @author eñaut a.a.
+ *
+ */
 public class ControladorPrincipal implements ActionListener{
+	/**
+	 * los atributos del controlador son las ventanas y modelos a las que accede
+	 */
 	private Principal frmPrincipal;
 	
+	/**
+	 * El controlador recibirá como parametro las vistas y modelos que va a controlar
+	 * 
+	 * @param principal jFrame que va a ser controlado (escuchado)
+	 */
 	public ControladorPrincipal(Principal principal) {
+		/*
+		 * inicializaremos las ventanas y los modelos
+		 */
 		frmPrincipal = principal;
+		
+		/*
+		 * asignaremos los eventos a los botones
+		 */
 		frmPrincipal.btnGestionarProductos.addActionListener(this);
 	}
 	
+	/**
+	 * Una especie de reset de la vista
+	 * aqui tambien se podrá acceder al modelo si se quiere mostrar algún dato
+	 * cuando se abra la ventana
+	 */
 	public void inicializar() {
 		frmPrincipal.setTitle("Desktop");
 		frmPrincipal.setLocationRelativeTo(null);
 		frmPrincipal.setVisible(false);
 	}
 
+	/**
+	 * Cuando algún elemento de la vista controlada por esta clase es accionada 
+	 * por ejemplo clic de boton etc, se ejecutará este método
+	 */
 	@Override
 	public void actionPerformed(ActionEvent e) {
+		//con este if sabremos que boton ha sido clicado
 		if(e.getSource() == frmPrincipal.btnGestionarProductos) {
 			ProductoModelo productoM = new ProductoModelo();
 			vista.ProductoFormulario productoF = new ProductoFormulario(frmPrincipal, true);
